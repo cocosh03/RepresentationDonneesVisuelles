b75a047e4958705fe6c0e9b959a052c79875a87f
Corentin <lexa5u@fst-ats-310-l13.fst.site.univ-lorraine.fr>
2019-01-22 12:12:49 +0100

    TP3

--- TP3/imagePhoto.m
+++ TP3/imagePhoto.m
@@ -3,3 +3,15 @@ imshow(im);
 im = double(rgb2gray(im))/255;
 figure;
 imshow (im);
+
+imf = fft2(im);
+imf = fftshift(imf);
+
+t = linspace(0, 2*pi, 1000);
+h = size(im,1);
+w = size(im,2);
+BW = poly2mask(10*cos(t)+w/2, 10*sin(t)+h/2, h , w);
+
+imf_lf = imf.*BW; %ou 1-BW pour les basse fréquences.
+imf_lf = fftshift(imf_lf);
+im = ifft2(imf_lf);
